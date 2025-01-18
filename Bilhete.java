4833c6883cbfd7e1e2396060764584ad062a8375
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-01-17 23:25:28 -0300

    Adição da lista de tarefas e criação da Classe Bilhete

---
+++ Bilhete.java
@@ -0,0 +1,10 @@
+public class Bilhete {
+
+    private int[] numerosEscolhidos;
+    private int[] resultadoSorteio;
+
+    public Bilhete(int[] numerosEscolhidos, int[] resultadoSorteio){
+        this.numerosEscolhidos = numerosEscolhidos;
+    }
+
+}
